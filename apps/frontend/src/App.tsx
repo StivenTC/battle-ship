1e1e84a7e00ee6e2f264055e295d5d01ed632973
Stiven Tovar <stivent367@gmail.com>
2025-12-26 11:53:44 -0500

    fix: tailwind classes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Grid } from "./components/game/Grid/Grid";
 import { SocketProvider } from "./context/SocketContext";
+import styles from "./App.module.scss";
 import "./index.scss";
 
 function App() {
   return (
     <SocketProvider>
-      <div>
-        <h1 className="text-3xl font-bold mb-8">Battleship</h1>
+      <div className={styles.appWrapper}>
+        <h1 className={styles.appHeader}>Battleship</h1>
         <Grid />
       </div>
     </SocketProvider>
